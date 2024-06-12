627a40ed1044eb45e187b3cfe8545895125bfea4
HarshitSoniAiLusion <harshit@ailusion.in>
2024-06-12 14:58:30 +0530

    add the signin functionality

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,7 @@ import user from '../models/userModel.js';
 import { errorHandler } from '../utils/errorHandler.js';
 import bcrypt from 'bcrypt'
 import validateEmail from '../utils/isValidEmail.js';
+import jwt from 'jsonwebtoken';
 
 export const signup=async(req,res,next)=>{
     if(!req.body.username || !req.body.email || !req.body.password || req.body.username==='' || req.body.email==='' || req.body.password===''){
@@ -25,9 +26,36 @@ export const signup=async(req,res,next)=>{
             password:hashedPassword
         });
         await newUser.save();
-        res.send({JSON,msg:'User SignUp SuccessFully'});
+        const token=jwt.sign({id:newUser._id},process.env.JWT_SECRET);
+        const {password:pass,...rest}=newUser._doc;
+        res.status(200).cookie('aiLusion_token',token,{httpOnly:true},{expiresIn:'7d'}).json(rest);
     }
     catch(err){
         next(err);
+    }
+}
+
+export const signin = async(req,res,next)=>{
+    if(!req.body.email || !req.body.password || req.body.username==='' || req.body.password===''){
+        next(errorHandler(500,'Email and password both are required'));
+        return;
+    }
+    try{
+        const currUser=await user.findOne({email:req.body.email});
+        if(!currUser){
+            next(errorHandler(400,'User Not Exist'));
+            return;
+        }
+        let validatePassword = bcrypt.compareSync(req.body.password,currUser.password);
+        if(!validatePassword){
+            return next(errorHandler(500,'Incorrect Password'));
+        }
+        const {password:pass,...rest}=currUser._doc;
+        const token=jwt.sign({id:currUser._id},process.env.JWT_SECRET);
+        res.status(200).cookie('aiLusion_token',token,{httpOnly:true},{expiresIn:'7d'}).json(rest);
+    } 
+    catch(err){
+        next(err);
+        return;
     }
 }
